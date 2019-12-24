d878ce1ca5d9bd34a8ae9a617ff4d4a38625d607
Ms <ms@juliver.com>
2019-12-24 16:40:09 +0100

    Première version

---
+++ Screenshot.java
@@ -0,0 +1,21 @@
+package ms.fr;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class Screenshot {
+	public static void takeSnapShot(WebDriver webdriver, String fileWithPath) throws Exception {
+		// Convert web driver object to TakeScreenshot
+		TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
+		// Call getScreenshotAs method to create image file
+		File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+		// Move image file to new destination
+		File DestFile = new File(fileWithPath);
+		// Copy file at destination
+		FileUtils.copyFile(SrcFile, DestFile);
+	}
+}
---
+++ dependency.md
@@ -0,0 +1,5 @@
+<dependency>
+	<groupId>ru.yandex.qatools.ashot</groupId>
+	<artifactId>ashot</artifactId>
+	<version>1.5.3</version>
+</dependency>
